8eba156e5b7ae45302a7f405cf2c16740fac90a4
Vinicius Miguel <viniciusmiguel2018@hotmail.com>
2024-03-08 21:32:42 -0300

    Primeiro commit

---
+++ src/components/Banner/index.js
@@ -0,0 +1,14 @@
+import styles from './Banner.module.css';
+
+function Banner({ img }){
+    return(
+        <div 
+            className={styles.banner} 
+            style={{ backgroundImage: `url('/images/banner-${img}.png')` }}
+
+        ></div>
+    )
+
+}
+
+export default Banner;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import style from './Footer.module.css';
+
+function Footer(){
+    return(
+        <>
+        <footer className={style.footer}>
+            <he>VMFlix &copy; Desenvolvido por Vinicius Miguel</he>
+        </footer>
+        </>
+    )
+}
+
+export default Footer;
